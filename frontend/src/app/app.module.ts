2a4479f2ef10382603fe2559eb5f6b67f3c6e309
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-04-07 20:38:55 -0300

    #3 - creating dashboard page template

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 import { NgModule } from '@angular/core';
+import { UserInfoCardComponent } from './components/user-info-card/user-info-card.component';
+import { UsersTableComponent } from './components/users-table/users-table.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    UserInfoCardComponent,
+    UsersTableComponent,
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatButtonModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatTableModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
